e2605011ef6d320484aaeabc6e299e9fa384720f
voiceu <voiceu@qq.com>
2022-05-02 15:11:18 +0800

    chore(jira_devtool): install jira_devtool backend

--- src/index.tsx
+++ src/index.tsx
@@ -1,28 +1,25 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom'
-
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-
-// const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// )
+import { loadDevTools } from 'jira-dev-tool'
 
 // 就是这里，尼玛，这种脚手架直接创建的入口形式，会有两次渲染
 import ReactDOM from 'react-dom/client'
+
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(
-  // 不用这个严格模式吧，之前的代码都是直接包裹路由，也没用严格模式
-  // react 17的时候严格模式不会render2遍，react 18会
-  <App />
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
-)
+
+// 用开发工具包起来，就能显示后端工具了
+loadDevTools(() => {
+  root.render(
+    // 不用这个严格模式吧，之前的代码都是直接包裹路由，也没用严格模式
+    // react 17的时候严格模式不会render2遍，react 18会
+    <App />
+
+    // <React.StrictMode>
+    //   <App />
+    // </React.StrictMode>
+  )
+})
 
 // 这样，用之前的形式才不会有两次渲染请求或者说hooks两次调用，但是这样会有警告
 // Warning: ReactDOM.render is no longer supported in React 18.
