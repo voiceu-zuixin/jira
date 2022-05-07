eae608aaa66905acac1b0d37801f0267ff547360
voiceu <voiceu@qq.com>
2022-05-07 08:42:42 +0800

    chore(antd_render): antd_Table_columns_render_function_notes

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -29,6 +29,9 @@ export const List = ({ list, users }: ListProps) => {
         {
           title: '负责人',
           // render函数具体参数的意义，看一下之前记录的帖子，还有官方文档
+          // 如果前面有dataIndex，那么第一个参数就是dataIndex对应的value作为key所对应值
+          // 如果没有，那么第一第二都是一样的数据
+          // 第三个参数是dataIndex
           render(project) {
             return <span>{users.find((user) => user.id === project.personId)?.name || '未知'}</span>
           }
