6bd9debf3bc591ad80499180edbb41ed4c36fbf0
voiceu <voiceu@qq.com>
2022-05-07 19:52:19 +0800

    fix(warn): <Table/>组件的key警告

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -17,9 +17,16 @@ interface ListProps {
   users: User[]
 }
 
+// 一个生成随机的key的函数
+const randomKey = () => {
+  return 10 * Math.random()
+}
+
 export const List = ({ list, users }: ListProps) => {
   return (
     <Table
+      // Table组件必须要有不同的key，这里暂时写一个随机的key函数
+      rowKey={randomKey}
       pagination={false}
       columns={[
         {
